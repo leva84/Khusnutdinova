f73b11d70cba6378d58c7390e15a144e8837680a
leva84 <denfrut8@gmail.com>
2020-05-09 14:38:16 +0300

    Update views for MAC and Trainings

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,6 @@ class Card < ApplicationRecord
   has_many :playing_cards
 
   validates :name, presence: true, uniqueness: true
-  #validates :photo, presence: true
+  validates :photo, presence: true
   validates :deck_name, presence: true
 end
