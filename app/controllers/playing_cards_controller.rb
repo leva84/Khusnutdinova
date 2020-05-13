f4c86d9a75688b0221778f64191035e3d4494b12
leva84 <denfrut8@gmail.com>
2020-05-13 06:30:34 +0300

    Refactoring Playing_cards controller

--- app/controllers/playing_cards_controller.rb
+++ app/controllers/playing_cards_controller.rb
@@ -1,35 +1,6 @@
 class PlayingCardsController < ApplicationController
-  before_action :set_playing_card, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only: [:edit, :update]
-
-  # GET /playing_cards
-  # GET /playing_cards.json
-  def index
-    @playing_cards = PlayingCard.where(game_id: params[:game_id])
-    @game = Game.find(params[:game_id])
-  end
-
-  # GET /playing_cards/1
-  def show
-  end
-
-  # GET /playing_cards/new
-  def new
-    @playing_card = PlayingCard.new
-  end
-
-  # GET /playing_cards/1/edit
-  def edit
-  end
-
-  # POST /playing_cards
-  # POST /playing_cards.json
-  def create
-    @playing_card = PlayingCard.new(playing_card_params)
-
-    @playing_card.save!
-    redirect_to @playing_card
-  end
+  before_action :set_playing_card
+  before_action :authenticate_user!
 
   # PATCH/PUT /playing_cards/1
   def update
